3faf5a7df9752aa51b4f6a35969784a1efbf37f2
Henry Jacobs <henryjacobscode@gmail.com>
2023-03-20 16:11:37 -0400

    add usenavigate to homebutton in sidebar

--- src/components/sidebar/sidebar-comps/general-nav-section.js
+++ src/components/sidebar/sidebar-comps/general-nav-section.js
@@ -1,12 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import Category from "./category";
 import { general_nav_sidebar_images } from "../../utils/export-image-objects";
 
 function GeneralNavSection () {
+
+    const navigate = useNavigate() ;
+
+    const navigate_home = () => {
+        navigate("/") ; 
+    }
+    
     return (
         <div>
-            <Category 
+            <Category
             icon={general_nav_sidebar_images.home_icon}
             text="Home"
+            click={navigate_home}
             />
             <Category
             icon={general_nav_sidebar_images.subscriptions_icon} 
