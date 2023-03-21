42ba5536071d839b3924571b1f8e753132f2fd78
Henry Jacobs <henryjacobscode@gmail.com>
2023-03-21 13:44:38 -0400

    Add function that takes user back to their homepage

--- src/components/sidebar/sidebar-comps/profile-related-links-section.js
+++ src/components/sidebar/sidebar-comps/profile-related-links-section.js
@@ -1,7 +1,24 @@
 import Category from "./category";
+import {getDoc, doc} from "firebase/firestore"
+import { firestore } from "../../../firebase/firebase";
 import { sidebar_profile_related_links_images } from "../../utils/export-image-objects";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { userContext } from "../../utils/contexts";
 
 function ProfileRelatedLinks () {
+
+    let navigate = useNavigate() ;
+    const {user, setUser} = useContext(userContext) ;  
+
+    const go_to_your_page = async () => {
+        let channel_information = (await getDoc(doc(firestore, "users", user.uid))).data() ; 
+        console.log(channel_information)
+        navigate(`/channel-page/${user.channel_name}`, {state : {
+            channel_information : channel_information, 
+        }})
+    }
+
     return (
         <div>
             <Category 
@@ -11,6 +28,7 @@ function ProfileRelatedLinks () {
                 <Category 
                 icon={sidebar_profile_related_links_images.your_videos_icon}
                 text="Your Videos"
+                click={go_to_your_page}
                 />
                 <Category 
                 icon={sidebar_profile_related_links_images.clock_icon}
