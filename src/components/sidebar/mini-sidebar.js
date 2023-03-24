a187a6070eb739ed890941d595c5ef1c3ae5694d
Hankus Pankus <henryjacobscode@gmail.com>
2023-03-24 10:03:59 -0400

    Add functionality to mini-sidebar buttons

--- src/components/sidebar/mini-sidebar.js
+++ src/components/sidebar/mini-sidebar.js
@@ -1,24 +1,52 @@
+import { useNavigate } from "react-router-dom"
+import { useContext } from "react";
+import { firestore } from "../../firebase/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { userContext } from "../utils/contexts";
 import { mini_sidebar_icons } from "../utils/export-image-objects"
 import MiniCategory from "./sidebar-comps/mini-category"
 
 function MiniSideBar () {
+
+    const navigate = useNavigate() ; 
+    const {user, setUser} = useContext(userContext) ;  
+
+    const go_home = () => { navigate("/") } ; 
+
+    const show_subscribers = () => { navigate("/subscriptions") } ; 
+
+    const show_profile = async () => {
+        let channel_information = (await getDoc(doc(firestore, "users", user.uid))).data() ; 
+        console.log(channel_information)
+        navigate(`/channel-page/${user.channel_name}`, {state : {
+            channel_information : channel_information, 
+        }})
+        window.location.reload() ; 
+    }
+
+    const show_about = () => { navigate("/about") } ;
+
     return (
         <div className="mini-sidebar">
             <MiniCategory 
             icon={mini_sidebar_icons.home_icon}
             text="Home"
+            click={go_home}
             />
             <MiniCategory 
             icon={mini_sidebar_icons.subscriptions_icon}
             text="Subscriptions"
+            click={show_subscribers}
             />
             <MiniCategory 
             icon={mini_sidebar_icons.profile_icon}
             text="Profile"
+            click={show_profile}
             />
             <MiniCategory 
             icon={mini_sidebar_icons.about_icon}
             text="About"
+            click={show_about} 
             />
         </div>
     )
--- src/components/sidebar/sidebar-comps/mini-category.js
+++ src/components/sidebar/sidebar-comps/mini-category.js
@@ -1,7 +1,7 @@
 
 function MiniCategory (props) {
     return (
-        <div className="mini-category">
+        <div className="mini-category" onClick={props.click}>
            <img className="mini-category-icon" src={props.icon} alt="mini-category-icon" /> 
            <div className="mini-cat-text">{props.text}</div>
         </div>
